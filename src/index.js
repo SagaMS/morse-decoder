1b011b003d8194d7a851b21f368afb621e396843
SagaMS <91miablond@gmail.com>
2024-09-13 00:27:43 +0700

    init:solved

--- src/index.js
+++ src/index.js
@@ -38,8 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
+
+    function decodeSingleWord(word){
+        let w = word.match(/.{1,2}/g);
+        let result =''
+        for (let i= 0; i < w.length; i++) {
+            if(w[i] ==='11'){
+                result +='-'
+            }else if(w[i]==='10'){
+                result +='.'
+            }else{
+                result+=''
+            }
+            
+        }
+        return MORSE_TABLE[result]
+    }
+       
     // write your solution here
+
+let words = expr.match(/.{1,10}/g);
+let wordMorse = []
+
+for (let index = 0; index < words.length; index++) {
+    if(words[index] === '**********'){
+        wordMorse.push(' ')
+    }else{
+        wordMorse.push(decodeSingleWord(words[index]))
+    }
+    
 }
+
+return wordMorse.join('')
+
+}
+
 
 module.exports = {
     decode
